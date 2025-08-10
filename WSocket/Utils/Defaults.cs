1e88f60986e03ba86fbca63fb8a325fc8c263f97
gonzobard777 <gonzo.bard@dwfe.ru>
2025-08-10 18:24:30 +0300

    .

--- WSocket/Utils/Defaults.cs
+++ WSocket/Utils/Defaults.cs
@@ -2,7 +2,12 @@
 
 public static class Defaults
 {
+    // ТипСообщения, количество байт.
     public const byte MessageTypeBytesLength = 2;
+
+    // ТипГруппы, количество байт.
     public const byte GroupTypeBytesLength = 2;
+
+    // ИдГруппы, количество байт.
     public const byte GroupIdBytesLength = 50;
 }
